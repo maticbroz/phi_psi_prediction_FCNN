8d581eb72aa582596ced1ea735955312d2dacd68
maticbroz <66975641+maticbroz@users.noreply.github.com>
2023-03-30 14:22:57 +0200

    Update model.py

--- Model/model.py
+++ Model/model.py
@@ -1,13 +1,18 @@
+import os
 import pandas as pd
 import tensorflow as tf
 from keras import models, layers, optimizers
 from keras import backend as K
-import tensorflow as tf
+
+# Constants
+TRAIN_DATA_PATH = "../Inputs/train_data.csv"
+VAL_DATA_PATH = "../Inputs/val_data.csv"
+TEST_DATA_PATH = "../Inputs/test_data.csv"
 
 # Import train and test data
-train = pd.read_csv("../Inputs/train_data.csv", delimiter=",")
-val = pd.read_csv("../Inputs/val_data.csv", delimiter=",")
-test = pd.read_csv("../Inputs/test_data.csv", delimiter=",")
+train = pd.read_csv(TRAIN_DATA_PATH, delimiter=",")
+val = pd.read_csv(VAL_DATA_PATH, delimiter=",")
+test = pd.read_csv(TEST_DATA_PATH, delimiter=",")
 
 # Inputs
 X_train = train.iloc[:, 5:]
@@ -47,3 +52,9 @@ model.compile(optimizer=optimizers.Adadelta(learning_rate=0.5),
 
 callback = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', patience=2,factor=0.01, mode="auto", min_delta=0.1,cooldown=0,min_lr=10**-15)
 history = model.fit(X_train, y_train,validation_data=(X_val, y_val),epochs=50,batch_size=2**8, callbacks=[callback])
+
+# Save the model
+model.save("model.h5")
+
+# Print the model summary
+model.summary()
